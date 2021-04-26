e5cbcab54afc051beacf895403fc7370f8695bcd
Kleeer <delhomme.claire.ts7@gmail.com>
2021-04-26 13:35:08 +0200

    lil cleanup

--- src/main/scala/Chat/Tree.scala
+++ src/main/scala/Chat/Tree.scala
@@ -35,7 +35,6 @@ object Tree {
       case Hungry() => "Pas de soucis, nous pouvons notamment vous offrir des croissants faits maisons !"
       //Added
       case Login(name) => "Bonjour " + name.tail + " !"
-        //TODO auth
       case Order(n, product) => "%d %s %s".format(n, product.productType, product.brand)
       case And(orderL, orderR) => "%s et %s".format(orderL.reply, orderR.reply)
       case Or(orderL, orderR) => orderL match {
@@ -43,10 +42,12 @@ object Tree {
         case _ => orderR.reply;
       }
       case Info(order) => "Cela coûte CHF %.1f".format(order.computePrice)
-      case Balance() => "Le montant actuel de votre solde est de CHF %.1f".format(0f)
+      //TODO auth
       case ComplexOrder(order) => "Voici donc %s! Cela coûte CHF %.1f et votre nouveau solde est de CHF %.1f."
         .format( order.reply, order.computePrice ,0f)
+      case Balance() => "Le montant actuel de votre solde est de CHF %.1f".format(0f)
     }
+
   }
 
   /**
@@ -57,11 +58,12 @@ object Tree {
   case class Hungry() extends ExprTree
   // Added
   case class Login(name: String) extends ExprTree
+  case class Product(productType : ProductType, brand : String) extends ExprTree
   case class Order(n: Int, product: Product) extends ExprTree
   case class And(orderL: Order, orderR: Order) extends ExprTree
   case class Or(orderL: Order, orderR: Order) extends ExprTree
-  case class Product(productType : ProductType, brand : String) extends ExprTree
-  case class Info(complex_order: ExprTree) extends ExprTree
-  case class Balance() extends ExprTree
+
+  case class Info(complexOrder: ExprTree) extends ExprTree
   case class ComplexOrder(complexOrder: ExprTree) extends ExprTree
+  case class Balance() extends ExprTree
 }
